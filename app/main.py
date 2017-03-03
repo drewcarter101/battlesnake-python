36e5ee11bdca658d46af48c78b2831fb21e50cb7
Sharon <sharongirl21@gmail.com>
2017-03-03 13:10:41 -0800

    testing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import bottle
 import os
 import random
+import pygame
+from pygame.locals import *
+import math
 
 
 @bottle.route('/static/<path:path>')
@@ -33,6 +36,11 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+
+    directions = ['up', 'down', 'right', 'left']
+
+    currentdir = 'left'
+
     ourSnakeX = data['snakes'[0]['coords'][0][0]]
     ourSnakeY = data['snakes'[0]['coords'][0][1]]
 
@@ -41,22 +49,57 @@ def move():
     downSpace = board_height - ourSnakeY
     upSpace = 0 - ourSnakeY
 
-    #enemyDistanceLeft
-    #enemyDistanceRight
-    #enemyDistanceUp
-    #enemyDistanceDown
+    #if upSpace == 0:
+     #   direction = 'left'
+    #if leftSpace == 0:
+     #   direction = 'up'
+    #if downSpace == 0:
+     #   direction = 'left'
+    #if rightSpace == 0:
+     #   direction = 'up'
 
-    if(upSpace == 0):
-        direction='left'
-    if(leftSpace==0):
-        direction='up'
-    if(downSpace==0):
-        direction='left'
-    if(rightSpace==0):
-        direction='up'
+    playerPos = data['snakes'[0]['coords']] + data['snakes'[1]['coords']] + data['snakes'[2]['coords']] + data['snakes'[3]['coords']] 
+    obstacles = [-1, -1] + [board_width + 1, -1] + [board_width + 1, board_height + 1] + [-1, board_height + 1]
+
+    def pathlen (self, a, b):
+        return int( ((a[0]-b[0])**2 + (a[1]-b[1])**2 )**0.5)
+
+    def coorAdd(self, pos, pat):
+        if pat == 'left':
+            pat = [-1, 0]
+        elif pat == 'right':
+            pat = [1, 0]
+        elif pat == 'up':
+            pat = [0, -1]
+        elif pat == 'down':
+            pat = [0, 1]
+        
+        return pos[0]+pat[0], pos[1]+pat[1]
+
+
+    olddir = currentdir
+    #position = data['snakes'[0]['coords'][0]]
+    position=[ourSnakeX, ourSnakeY]
+
+    complement = [('up', 'down'), ('down', 'up'), ('right', 'left'), ('left', 'right')]
+    invaliddir = [x for (x, y) in complement if y == olddir]
+    validdir = [dir for dir in directions if not(dir in invaliddir)]
+
+    validdir=[dir for dir in validdir if not (coorAdd(position,dir) in obstacles or coorAdd(position,dir) in playerPos)]
+    olddir= olddir if olddir in validdir or len(validdir)==0 else validdir[0]
+    shortest=self.pathlen(coorAdd(position,olddir) , data['food'])
+
+    for dir in validdir:
+            newpos=self.coorAdd(position,dir)
+            newlen=self.pathlen(newpos , game.foodpos)#length in shortest path
+            if newlen < shortest:
+                if not ( newpos in obstacles or newpos in playerPos):
+                    olddir=dir
+                    shortest=newlen
+    currentdir = olddir
 
     return {
-        'move': direction,
+        'move': currentdir,
         'taunt': 'Everything is Awesome!'
     }
 
